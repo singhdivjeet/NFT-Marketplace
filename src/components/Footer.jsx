11e7c5039e8d4bbc9cae419ade8b1d70381b07fd
singhdivjeet <divjeet1@gmail.com>
2021-11-22 17:23:34 -0800

    minor UI fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
 		<footer className="bg-pink-600 text-coolGray-100 fixed w-full bottom-0 z-10">
-			<div class="grid justify-center p-1 lg:justify-between">
+			<div class="grid justify-center px-6 p-1 lg:justify-between">
 				<div class="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
 					<span>©2021 All rights reserved</span>
 					<a href="#">
